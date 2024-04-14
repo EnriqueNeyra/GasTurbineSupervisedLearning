25a1a37313148f86a65a29e2c14884d7ebc60b05
Enrique Neyra <94760483+EnriqueNeyra@users.noreply.github.com>
2024-04-14 11:31:07 -0700

    Add comments to outline steps

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ y = gas_turbine_co_and_nox_emission_data_set.data.targets
 # metadata
 print(gas_turbine_co_and_nox_emission_data_set.metadata)
 print("---------------------------------------------------------------------------------------------------------------")
-# features table structure
+# features table structure (TEY is the target variable)
 print(X)
 print("---------------------------------------------------------------------------------------------------------------")
+
+# 1. need to split dataset into first 3 years (training) and the last 2 years (testing)
+# 2. input training dataset into machine learning model
+#       - Regression will predict continuous value (exact TEY value)
+#       - Classification can predict category (different ranges of TEY values such as low, mid, high)
+# 3. input test dataset into model and evaluate performance
